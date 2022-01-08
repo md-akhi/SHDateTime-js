67f2864239f1d655d5faff9b07fcf3db9ae7e336
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2022-01-08 22:45:13 +0330

    chore: add utc & convert to class

--- SHDate.js
+++ SHDate.js
@@ -7,16 +7,22 @@
  * @license https://www.gnu.org/licenses/agpl-3.0.en.html AGPL-3.0 License
  * @version Release: 1.0.0
  */
+class SHWord {}
 
-export default (() => {
-	function SHDate() {
+class SHDate {
+	#shDate;
+	#shDateUTC;
+	#date;
+	#now;
+	#utc;
+	constructor() {
 		if (!this) {
-			let date = new SHDate(arguments);
-			return date.toString();
+			return new SHDate(arguments).toString();
 		}
-		this.date = new Date();
-		Update(this);
-		UpdateUTC(this);
+		this.#date = new Date();
+		this.#now = Date.now();
+		this.#utc = Date.UTC(this.#date.getUTCFullYear());
+		this.#Update();
 		if (typeof arguments[0] == "number" && arguments.length == 1) {
 			// value
 			if (arguments[0].length == 4) {
@@ -37,196 +43,32 @@ export default (() => {
 			// dateString
 			throw new Error("Not Implemented dateString");
 		}
-
-		this.now = Now();
-		// setter
-		this.setFullYear = FullYearSet.bind(this);
-		this.setMonth = MonthSet.bind(this);
-		this.setDate = DateSet.bind(this);
-		this.setHours = HoursSet.bind(this);
-		this.setMinutes = MinutesSet.bind(this);
-		this.setSeconds = SecondsSet.bind(this);
-		this.setMilliseconds = MillisecondsSet.bind(this);
-		this.setTime = TimeSet.bind(this);
-		//getter
-		this.getFullYear = FullYearGet.bind(this);
-		this.getMonth = MonthGet.bind(this);
-		this.getDate = DateGet.bind(this);
-		this.getHours = HoursGet.bind(this);
-		this.getMinutes = MinutesGet.bind(this);
-		this.getSeconds = SecondsGet.bind(this);
-		this.getMilliseconds = MillisecondsGet.bind(this);
-		this.getTime = TimeGet.bind(this);
-		this.getTimezoneOffset = TimezoneOffsetGet.bind(this);
-
-		// UTC
-		this.UTC = UTC();
-		// setter UTC
-		this.setUTCFullYear = FullYearUTCSet.bind(this);
-		this.setUTCMonth = MonthUTCSet.bind(this);
-		this.setUTCDate = DateUTCSet.bind(this);
-		this.setUTCHours = HoursUTCSet.bind(this);
-		this.setUTCMinutes = MinutesUTCSet.bind(this);
-		this.setUTCSeconds = SecondsUTCSet.bind(this);
-		this.setUTCMilliseconds = MillisecondsUTCSet.bind(this);
-		this.setUTCTime = TimeUTCSet.bind(this);
-		// getter UTC
-		this.getUTCFullYear = FullYearUTCGet.bind(this);
-		this.getUTCMonth = MonthUTCGet.bind(this);
-		this.getUTCDate = DateUTCGet.bind(this);
-		this.getUTCHours = HoursUTCGet.bind(this);
-		this.getUTCMinutes = MinutesUTCGet.bind(this);
-		this.getUTCSeconds = SecondsUTCGet.bind(this);
-		this.getUTCMilliseconds = MillisecondsUTCGet.bind(this);
-		this.getUTCTime = TimeUTCGet.bind(this);
+		console.log(this);
 	}
 
-	function Update(tis) {
-		tis.shDate = GregorianToSolar(
-			tis.date.getFullYear(),
-			tis.date.getMonth() + 1,
-			tis.date.getDate()
-		);
-	}
-	function UpdateUTC(tis) {
-		tis.shDateUTC = GregorianToSolar(
-			tis.date.getUTCFullYear(),
-			tis.date.getUTCMonth() + 1,
-			tis.date.getUTCDate()
-		);
-	}
-
-	function FullYearSet(year, month, date) {
-		const [gyear, gmonth, gdate] = SolarToGregorian(year, month, date);
-		return this.date.setFullYear(gmonth - 1, gdate, gyear);
-	}
-	function MonthSet(month, date) {
-		const [gyear, gmonth, gdate] = SolarToGregorian(
-			this.getFullYear(),
-			month,
-			date
-		);
-		return this.date.setMonth(gmonth - 1, gdate);
-	}
-	function DateSet(date) {
-		const [gyear, gmonth, gdate] = SolarToGregorian(
-			this.getFullYear(),
-			this.getMonth(),
-			date
-		);
-		return this.date.setDate(gdate);
-	}
-	function FullYearGet() {
-		if (this.date[0] == this.date.getFullYear()) return this.shDate[0];
-		Update(this);
-		return this.shDate[0];
-	}
-	function MonthGet() {
-		if (this.date[1] == this.date.getMonth() + 1) return this.shDate[1];
-		Update(this);
-		return this.shDate[1];
-	}
-	function DateGet() {
-		if (this.date[2] == this.date.getDate()) return this.shDate[2];
-		Update(this);
-		return this.shDate[2];
-	}
-
-	function HoursSet(hours, minutes, seconds, milliSeconds) {
-		return this.date.setHours(hours, minutes, seconds, milliSeconds);
-	}
-	function MinutesSet(minutes, seconds, milliSeconds) {
-		return this.date.setMinutes(minutes, seconds, milliSeconds);
-	}
-	function SecondsSet(seconds, milliSeconds) {
-		return this.date.setSeconds(seconds, milliSeconds);
-	}
-	function MillisecondsSet(milliSeconds) {
-		return this.date.setMilliseconds(milliSeconds);
-	}
-	function TimeSet(time) {
-		return this.date.setTime(time);
-	}
-	function HoursGet() {
-		return this.date.getHours();
-	}
-	function MinutesGet() {
-		return this.date.getMinutes();
-	}
-	function SecondsGet() {
-		return this.date.getSeconds();
-	}
-	function MillisecondsGet() {
-		return this.date.getMilliseconds();
-	}
-	function TimeGet() {
-		return this.date.getTime();
-	}
-	function TimezoneOffsetGet() {
-		return this.date.getTimezoneOffset();
-	}
-	const Now = () => {
-		return Date.now();
-	};
-	const UTC = (year, month, day, hour, minute, second, millisecond) => {
-		return Date.UTC(year, month, day, hour, minute, second, millisecond);
-	};
-
-	SHDate.prototype = {
-		setFullYear: (year, month = false, date = false) => {
-			this.setFullYear(
-				year ?? this.getFullYear(),
-				month ?? this.getMonth(),
-				date ?? this.getDate()
+	#Update(isUTC = false) {
+		if (
+			parseInt(this.getTime() / 1000) != parseInt(this.#date.getTime() / 1000)
+		) {
+			if (isUTC) {
+				this.#shDateUTC = this.#GregorianToSolar(
+					this.#date.getUTCFullYear(),
+					this.#date.getUTCMonth() + 1,
+					this.#date.getUTCDate()
+				);
+				return;
+			}
+			this.#shDate = this.#GregorianToSolar(
+				this.#date.getFullYear(),
+				this.#date.getMonth() + 1,
+				this.#date.getDate()
 			);
-			return this;
-		},
-		setMonth: (month, date = false) =>
-			this.setMonth(month ?? this.getMonth(), date ?? this.getDate()),
-		setDate: (date) => this.setDate(date ?? this.getDate()),
-		setHours: (hours, minutes = false, seconds = false, milliSeconds = false) =>
-			this.setHours(
-				hours,
-				minutes ?? this.getMinutes(),
-				seconds ?? this.getSeconds(),
-				milliSeconds ?? this.getMilliseconds()
-			),
-		setMinutes: (minutes, seconds = false, milliSeconds = false) =>
-			this.setMinutes(
-				minutes,
-				seconds ?? this.getSeconds(),
-				milliSeconds ?? this.getMilliseconds()
-			),
-		setSeconds: (seconds, milliSeconds = false) =>
-			this.date.setSeconds(seconds, milliSeconds ?? this.getMilliseconds()),
-		setMilliseconds: (milliSeconds) => this.date.setMilliseconds(milliSeconds),
-		setTime: (time) => this.date.setTime(time),
-		getFullYear: () => this.getFullYear(),
-		getMonth: () => this.getMonth(),
-		getDate: () => this.getDate(),
-		getHours: () => this.getHours(),
-		getMinutes: () => this.getMinutes(),
-		getSeconds: () => this.getSeconds(),
-		getMilliseconds: () => this.getMilliseconds(),
-		getTime: () => this.getTime(),
-		getTimezoneOffset: () => this.getTimezoneOffset(),
-		getDay: () =>
-			DayOfWeek(this.getFullYear(), this.getMonth(), this.getDate()),
-		valueOf: () => this.getTime(),
-		now: () => this.now(),
-		UTC: () => this.UTC(),
-		checkDate: (year, month, date) => CheckDate(year, month, date),
-		checkTime: (hours, minutes, seconds) => CheckTime(hours, minutes, seconds),
-		toString: () => "Date.toString()",
-		/* // get timestamp in linux format
-		toString: function(format, convertDigit) {
-			var ret = (!!format && format !== null) ? jdate._format(format + '', this.jdate) : jdate._format('yyyy-MM-dd HH:mm:ss.l Z', this.jdate);
-			return (!!convertDigit && convertDigit !== null) ? ret.toFaDigit() : ret;
-		} */
-	};
-	SHDate.now = () => Date.now();
+			return;
+		}
+		return;
+	}
 
-	function GregorianToSolar(gyear, gmonth, gdate, julian) {
+	#GregorianToSolar(gyear, gmonth, gdate, julian) {
 		let doy, year, month, date, dim;
 		// if(julian&&(gyear<=1581||(gyear==1582&&gmonth<=10&&gdate<15)))
 		// list(gmonth,gdate,gyear)=explode('/', jdtogregorian(juliantojd(gmonth,gdate,gyear)));
@@ -235,11 +77,11 @@ export default (() => {
 			([0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334][gmonth] +
 				gdate) -
 			226745 + //226745 = 621*365+80
-			Math.abs(GIsLeap(gyear, 1) - IsLeap(gyear - 621, 1));
-		if (GIsLeap(gyear) && gmonth > 2) doy++;
+			Math.abs(this.#GIsLeap(gyear, 1) - this.#IsLeap(gyear - 621, 1));
+		if (this.#GIsLeap(gyear) && gmonth > 2) doy++;
 		year = parseInt(doy / 365) + 1;
 		date = doy % 365;
-		const jleap = IsLeap(year);
+		const jleap = this.#IsLeap(year);
 		if (gmonth < 4 && jleap && year == gyear - 622) date++;
 		const jmn = [0, 31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, jleap + 29];
 		for (let i in jmn) {
@@ -250,29 +92,43 @@ export default (() => {
 		if (date == 0) {
 			year--;
 			month = 12;
-			date = IsLeap(year) + 29;
+			date = this.#IsLeap(year) + 29;
 		}
 		return [parseInt(year), parseInt(month), parseInt(date)];
 	}
 
-	function SolarToGregorian(year, month, date, julian) {
+	#SolarToGregorian(year, month, date, julian) {
 		let gdoy, gyear, gmonth, gdate, dim;
 		gdoy =
 			(year - 1) * 365 +
-			DayOfYear(month, date, year) +
+			this.#DayOfYear(month, date, year) +
 			1 +
 			226745 /*621*365+80*/ -
-			Math.abs(GIsLeap(year + 621, 1) - IsLeap(year, 1));
+			Math.abs(this.#GIsLeap(year + 621, 1) - this.#IsLeap(year, 1));
 		gyear = parseInt(gdoy / 365) + 1;
 		gdate = gdoy % 365;
-		const prev_gleap = GIsLeap(gyear - 1);
-		const jleap = IsLeap(year);
+		const prev_gleap = this.#GIsLeap(gyear - 1);
+		const jleap = this.#IsLeap(year);
 		if (
 			(prev_gleap && gyear == year + 622) ||
 			(jleap && prev_gleap && month > 11)
 		)
 			gdate--;
-		gmn = [0, 31, GIsLeap(gyear) + 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+		const gmn = [
+			0,
+			31,
+			this.#GIsLeap(gyear) + 28,
+			31,
+			30,
+			31,
+			30,
+			31,
+			31,
+			30,
+			31,
+			30,
+			31,
+		];
 		for (let i in gmn) {
 			(gmonth = i), (dim = gmn[i]);
 			if (gdate <= dim) break;
@@ -291,13 +147,14 @@ export default (() => {
 		// list(gmonth,gdate,gyear)=explode('/', jdtojulian(gregoriantojd(gmonth,gdate,gyear)));
 		return [parseInt(gyear), parseInt(gmonth), parseInt(gdate)];
 	}
-	function IsLeap(year, all) {
+
+	#IsLeap(year, all) {
 		return all
 			? parseInt((year += 1128) * 365.2422) - parseInt(--year * 365.2422) - 365
 			: Math.ceil((year += 1127) * 365.2422 - year * 365) - 274;
 	}
 
-	function GIsLeap(gyear, all) {
+	#GIsLeap(gyear, all) {
 		return all
 			? gyear % 4 == 0 && !(gyear % 100 == 0 && gyear % 400 != 0)
 			: Math.ceil(
@@ -305,36 +162,38 @@ export default (() => {
 			  ) - 150;
 	}
 
-	function DayOfWeek(year, month, date) {
-		return ((1127 + year) * 365.2422 + DayOfYear(month, date, year) - 3) % 7;
-	}
-
-	function DayOfYear(month, date, year) {
+	#DayOfWeek(year, month, date) {
 		return (
-			((month < 7 ? (month - 1) * 31 : (month - 7) * 30 + 186) + --date) %
-			(DaysInYear(year) - 1)
+			((1127 + year) * 365.2422 + this.#DayOfYear(month, date, year) - 3) % 7
 		);
 	}
 
-	function WeekOfYear(year, month, date) {
+	#DayOfYear(month, date, year) {
+		return (
+			((month < 7 ? (month - 1) * 31 : (month - 7) * 30 + 186) + --date) %
+			(this.#DaysInYear(year) - 1)
+		);
+	}
+
+	#WeekOfYear(year, month, date) {
 		var iw, iy;
 		/* Find if Y M D falls in YearNumber --Y, WeekNumber 52 or 53 */
 		if (
-			(doy = DayOfYear(month, date, year) + 1) <=
-				8 - (far1weekday = DayOfWeek(year, 1, 1) + 1) &&
+			(doy = this.#DayOfYear(month, date, year) + 1) <=
+				8 - (far1weekday = this.#DayOfWeek(year, 1, 1) + 1) &&
 			far1weekday > 4
 		)
 			return [
 				(iw =
-					far1weekday == 5 || (far1weekday == 6 && IsLeap((iy = --year)))
+					far1weekday == 5 || (far1weekday == 6 && this.#IsLeap((iy = --year)))
 						? 53
 						: 52),
 				iy,
 			];
 		/* Find if Y M D falls in YearNumber ++Y, WeekNumber 1 */
 		if (
-			365 - doy + IsLeap(year) <
-			4 - (weekday = DayOfWeek(year, month, date) + 1)
+			365 - doy + this.#IsLeap(year) <
+			4 - (weekday = this.#DayOfWeek(year, month, date) + 1)
 		) {
 			iy = ++year;
 			return [(iw = 1), iy];
@@ -346,32 +205,255 @@ export default (() => {
 		return [iw, iy];
 	}
 
-	function DaysInMonth(year, month) {
-		return month < 7 ? 31 : month < 12 ? 30 : IsLeap(year) + 29;
+	#DaysInMonth(year, month) {
+		month < 7 ? 31 : month < 12 ? 30 : this.#IsLeap(year) + 29;
 	}
 
-	function DaysInYear(year) {
-		return IsLeap(year) + 365;
+	#DaysInYear(year) {
+		this.#IsLeap(year) + 365;
 	}
-	function CheckDate(year, month, date) {
+
+	now() {
+		return Date.now();
+	}
+	UTC(
+		year,
+		month = false,
+		day = false,
+		hour = false,
+		minute = false,
+		second = false,
+		millisecond = false
+	) {
+		let shDate = new SHDate();
+		let date = new Date();
+		const UTC = this.#SolarToGregorian(
+			year ?? shDate.getUTCFullYear(),
+			month ?? shDate.getUTCMonth() + 1,
+			day ?? shDate.getUTCDate()
+		);
+		console.log(
+			shDate.getUTCFullYear(),
+			shDate.getUTCMonth() + 1,
+			shDate.getUTCDate(),
+			UTC,
+			date.getUTCFullYear(),
+			date.getUTCMonth() + 1,
+			date.getUTCDate()
+		);
+		return Date.UTC(
+			UTC[0],
+			UTC[1] - 1,
+			UTC[2],
+			hour ?? date.getUTCHours(),
+			minute ?? date.getUTCMinutes(),
+			second ?? date.getUTCSeconds(),
+			millisecond ?? date.getUTCMilliseconds()
+		);
+	}
+
+	setFullYear(year, month = false, date = false) {
+		const [gyear, gmonth, gdate] = this.#SolarToGregorian(
+			year ?? this.getFullYear(),
+			month ?? this.getMonth(),
+			date ?? this.getDate()
+		);
+		return this.#date.setFullYear(gmonth - 1, gdate, gyear);
+	}
+	setUTCFullYear(year, month = false, date = false) {
+		const [gyear, gmonth, gdate] = this.#SolarToGregorian(
+			year ?? this.getUTCFullYear(),
+			month ?? this.getUTCMonth(),
+			date ?? this.getUTCDate()
+		);
+		return this.#date.setUTCFullYear(gmonth - 1, gdate, gyear);
+	}
+	setMonth(month, date = false) {
+		const [gyear, gmonth, gdate] = this.#SolarToGregorian(
+			this.getFullYear(),
+			month ?? this.getMonth(),
+			date ?? this.getDate()
+		);
+		return this.#date.setMonth(gmonth - 1, gdate);
+	}
+	setUTCMonth(month, date = false) {
+		const [gyear, gmonth, gdate] = this.#SolarToGregorian(
+			this.getUTCFullYear(),
+			month ?? this.getUTCMonth(),
+			date ?? this.getUTCDate()
+		);
+		return this.#date.setUTCMonth(gmonth - 1, gdate);
+	}
+	setDate(date) {
+		const [gyear, gmonth, gdate] = this.#SolarToGregorian(
+			this.getFullYear(),
+			this.getMonth(),
+			date ?? this.getDate()
+		);
+		return this.#date.setDate(gdate);
+	}
+	setUTCDate(date) {
+		const [gyear, gmonth, gdate] = this.#SolarToGregorian(
+			this.getUTCFullYear(),
+			this.getUTCMonth(),
+			date ?? this.getDate()
+		);
+		return this.#date.setUTCDate(gdate);
+	}
+	setHours(hours, minutes = false, seconds = false, milliSeconds = false) {
+		return this.#date.setHours(
+			hours,
+			minutes ?? this.getMinutes(),
+			seconds ?? this.getSeconds(),
+			milliSeconds ?? this.getMilliseconds()
+		);
+	}
+	setUTCHours(hours, minutes = false, seconds = false, milliSeconds = false) {
+		return this.#date.setUTCHours(
+			hours,
+			minutes ?? this.getUTCMinutes(),
+			seconds ?? this.getUTCSeconds(),
+			milliSeconds ?? this.getUTCMilliseconds()
+		);
+	}
+	setMinutes(minutes, seconds = false, milliSeconds = false) {
+		return this.#date.setMinutes(
+			minutes,
+			seconds ?? this.getSeconds(),
+			milliSeconds ?? this.getMilliseconds()
+		);
+	}
+	setUTCMinutes(minutes, seconds = false, milliSeconds = false) {
+		return this.#date.setUTCMinutes(
+			minutes,
+			seconds ?? this.getUTCSeconds(true),
+			milliSeconds ?? this.getUTCMilliseconds(true)
+		);
+	}
+	setSeconds(seconds, milliSeconds = false) {
+		return this.#date.setSeconds(
+			seconds,
+			milliSeconds ?? this.getMilliseconds()
+		);
+	}
+	setUTCSeconds(seconds, milliSeconds = false) {
+		return this.#date.setUTCSeconds(
+			seconds,
+			milliSeconds ?? this.getUTCMilliseconds()
+		);
+	}
+	setMilliseconds(milliSeconds) {
+		return this.#date.setMilliseconds(milliSeconds);
+	}
+	setUTCMilliseconds(milliSeconds) {
+		return this.#date.setUTCMilliseconds(milliSeconds);
+	}
+	getFullYear() {
+		this.#Update();
+		return this.#shDate[0];
+	}
+	getUTCFullYear() {
+		this.#Update(true);
+		return this.shDateUTC[0];
+	}
+	getMonth() {
+		this.#Update(this);
+		return this.#shDate[1];
+	}
+	getUTCMonth() {
+		this.#Update(true);
+		return this.shDateUTC[1];
+	}
+	getDate() {
+		this.#Update(this);
+		return this.#shDate[2];
+	}
+	getUTCDate() {
+		this.#Update(true);
+		return this.shDateUTC[2];
+	}
+	getHours() {
+		return this.#date.getHours();
+	}
+	getUTCHours() {
+		return this.#date.getUTCHours();
+	}
+	getMinutes() {
+		return this.#date.getMinutes();
+	}
+	getUTCMinutes() {
+		return this.#date.getUTCMinutes();
+	}
+	getSeconds() {
+		return this.#date.getSeconds();
+	}
+	getUTCSeconds() {
+		return this.#date.getUTCSeconds();
+	}
+	getMilliseconds() {
+		return this.#date.getMilliseconds();
+	}
+	getUTCMilliseconds() {
+		return this.#date.getUTCMilliseconds();
+	}
+	getDay() {
+		return this.#DayOfWeek(this.getFullYear(), this.getMonth(), this.getDate());
+	}
+	getUTCDay() {
+		return this.#DayOfWeek(
+			this.getFullYear(true),
+			this.getMonth(true),
+			this.getDate(true)
+		);
+	}
+	getTimezoneOffset() {
+		return this.#date.getTimezoneOffset();
+	}
+	setTime(time) {
+		//if (isUTC) return this.#date.setUTCTime(time);
+		return this.#date.setTime(time);
+	}
+	getTime() {
+		//if (isUTC) return this.#date.getUTCTime();
+		return this.#date.getTime();
+	}
+	valueOf() {
+		return this.getTime();
+	}
+	checkDate(year, month, date) {
 		return !(
 			year < 1 ||
 			year > 1700 /*3500000  3,500,000 */ ||
 			month < 1 ||
 			month > 12 ||
 			date < 1 ||
-			date > DaysInMonth(year, month)
+			date > this.#DaysInMonth(year, month)
 		);
 	}
-
-	function CheckTime(h, i, s) {
-		return !(h < 0 || h > 23 || i < 0 || i > 59 || s < 0 || s > 59);
+	checkTime(hours, minutes, seconds) {
+		return !(
+			hours < 0 ||
+			hours > 23 ||
+			minutes < 0 ||
+			minutes > 59 ||
+			seconds < 0 ||
+			seconds > 59
+		);
 	}
+	toString() {
+		return "Date.toString()";
+	}
+	parse() {
+		return "Date.parse()";
+	}
+	/* // get timestamp in linux format
+	toString: function(format, convertDigit) {
+		var ret = (!!format && format !== null) ? jdate._format(format + '', this.jdate) : jdate._format('yyyy-MM-dd HH:mm:ss.l Z', this.jdate);
+		return (!!convertDigit && convertDigit !== null) ? ret.toFaDigit() : ret;
+	} */
+}
 
-	return SHDate;
-})();
-
-function NumsTo(str, con = "FA", dec = false) {
+const NumsTo = (str, con = "FA", dec = false) => {
 	dec = dec || ",";
 	EN = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".");
 	FA = array("۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹", dec);
@@ -382,5 +464,7 @@ function NumsTo(str, con = "FA", dec = false) {
 		str = str_replace(lang, con, str);
 	}
 	return str;
-}
+};
+
 export { NumsTo };
+export default SHDate;
