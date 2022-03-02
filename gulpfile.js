31881d3ea9f2dcdc3efd01a4ad2d8f3bdfbbaf96
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2022-03-02 13:23:40 +0330

    feat: added gulp

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+
+'use strict';
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const del = require('del');
+
+/**
+ * Remove build directory.
+ */
+ gulp.task('clean', function () {
+  return del(["lib"]);
+});
+
+/**
+ * Copy commonjs package.
+ */
+gulp.task('package-cjs', function() {
+  return gulp.src(['build/script/package-cjs.json'])
+	.pipe(rename( {
+		basename: 'package'
+    }))
+    .pipe(gulp.dest('lib/src'))
+});
+
+/**
+ * Copy esm package.
+ */
+gulp.task('package-mjs', function() {
+  return gulp.src(['build/script/package-mjs.json'])
+	.pipe(rename( {
+		basename: 'package'
+    }))
+    .pipe(gulp.dest('lib/mjs'))
+});
+
+ /**
+	* Run default.
+	*/
+gulp.task('default', gulp.parallel('package-cjs', 'package-mjs'));
+
+
+
+
+
