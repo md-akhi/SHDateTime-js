1f9fa4c5c82bd827e585670dab6b4669ce0ba300
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2022-04-29 19:23:00 +0430

    chore: added combine ts & d.ts in types

--- gulpfile.js
+++ gulpfile.js
@@ -51,24 +51,27 @@ function browser() {
 	].join("\n");
 
 	return gulp
-		.src("dist/browser/shdatetime.js")
+		.src("dist/browser/shdatetime.js",{sourcemaps:true})
 		.pipe(babel({ presets: ["@babel/env"] }))
 		.pipe(banner(infoLong))
 		.pipe(gulp.dest("dist/browser"))
 		.pipe(uglify())
 		.pipe(rename({ extname: ".min.js" }))
 		.pipe(banner(infoShort))
-		.pipe(gulp.dest("./dist/browser"));
+		.pipe(gulp.dest("./dist/browser",{sourcemaps:"."}));
 }
 
 function delTSBrowser() {
-	return del(["src/browser"]);
+	return del(["src/browser","dist/browser/*.d.ts"]);
+}
+function moveTSBrowser() {
+	return gulp.src(["src/browser/*.ts","dist/browser/*.d.ts"]).pipe(gulp.dest("dist/types"));
 }
 
 /**
  * Run default.
  */
-exports.default = gulp.series(browser, delTSBrowser);
+exports.default = gulp.series(browser,moveTSBrowser, delTSBrowser);
 exports.combineTS = combineTS;
 exports.moveDTS = moveDTS;
 exports.delDTS = delDTS;
