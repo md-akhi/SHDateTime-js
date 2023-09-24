7f9ba948631d2faccc41beba5e297c6a14b68771
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2023-09-24 13:11:07 +0330

    fix(docs): fix live exam
    Signed-off-by: MD Amanalikhani <md.akhi.ir@gmail.com>
    Co-authored-by: MD Amanalikhani <md.akhi.ir@gmail.com>

--- docs/examples/code/data.js
+++ docs/examples/code/data.js
@@ -1,19 +1,20 @@
 const exData = function (ex) {
 	switch (ex) {
 		case "now":
-			return `// this example takes 2 seconds to run
-				const start = Date.now();
+			return `
+// this example takes 2 seconds to run
+const start = Date.now();
 
-				console.log("starting timer...");
-				// expected output: starting timer...
+console.log("starting timer...");
+// expected output: starting timer...
 
-				setTimeout(() => {
-					const millis = Date.now() - start;
-
-					console.log(\`seconds elapsed = \${Math.floor(millis / 1000)}\`);
-					// expected output: seconds elapsed = 2
-				}, 2000);`;
+setTimeout(() => {
+	const millis = Date.now() - start;
+	console.log(\`seconds elapsed = \${Math.floor(millis / 1000)}\`);
+	// expected output: seconds elapsed = 2
+}, 2000);
+				`;
 		default:
-			return "false";
+			return "// Cannot show default code";
 	}
 };
