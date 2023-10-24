01fd09a34d232d274d2936d728c68d67a6cfe36a
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2023-10-24 14:55:33 +0330

    refactor(gulp): بازنویسی gulp
    Signed-off-by: MD Amanalikhani <md.akhi.ir@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -161,7 +161,6 @@ function buildCjs(cb) {
 		}
 	);
 }
-gulp.task(buildCjs);
 
 function buildMjs(cb) {
 	//"tsc:mjs"
@@ -174,7 +173,6 @@ function buildMjs(cb) {
 		}
 	);
 }
-gulp.task(buildMjs);
 
 function buildBrowser(cb) {
 	//"tsc:browser"
@@ -187,7 +185,8 @@ function buildBrowser(cb) {
 		}
 	);
 }
-gulp.task(buildBrowser);
+
+gulp.task("buildTSC", gulp.parallel(buildCjs, buildMjs, buildBrowser));
 
 function devClean(cb) {
 	return exec(
@@ -201,40 +200,61 @@ function devClean(cb) {
 }
 gulp.task(devClean);
 
-function testsCJS(cb) {
-	return exec(" cd dist/tests/cjs/ && npm i", function (err, stdout, stderr) {
+function moveTestsCJS(cb) {
+	return gulp.src(["tests/cjs/*"]).pipe(gulp.dest("dist/tests/cjs"));
+}
+function installTestsCJS(cb) {
+	return exec("(cd dist/tests/cjs && npm i)", function (err, stdout, stderr) {
 		// console.log(stdout);
 		// console.log(stderr);
 		cb(err);
 	});
 }
-function testsMJS(cb) {
-	return exec(" cd ../mjs/ && npm i", function (err, stdout, stderr) {
+function moveTestsMJS(cb) {
+	return gulp.src(["tests/mjs/*"]).pipe(gulp.dest("dist/tests/mjs"));
+}
+function installTestsMJS(cb) {
+	return exec("(cd dist/tests/mjs && npm i)", function (err, stdout, stderr) {
 		// console.log(stdout);
 		// console.log(stderr);
 		cb(err);
 	});
 }
-function testsPKGS(cb) {
-	return exec(" cd ../pkgs/ && npm i ", function (err, stdout, stderr) {
+function moveTestsPKGS(cb) {
+	return gulp.src(["tests/pkgs/*"]).pipe(gulp.dest("dist/tests/pkgs"));
+}
+function installTestsPKGS(cb) {
+	return exec("(cd dist/tests/pkgs && npm i) ", function (err, stdout, stderr) {
 		// console.log(stdout);
 		// console.log(stderr);
 		cb(err);
 	});
 }
 function buildTests(cb) {
-	return (
-		exec("tsc --project tsconfig.test.json", function (err, stdout, stderr) {
+	return exec(
+		"tsc --project tsconfig.test.json",
+		function (err, stdout, stderr) {
 			// console.log(stdout);
 			// console.log(stderr);
 			cb(err);
-		}) &&
-		testsCJS &&
-		testsMJS &&
-		testsPKGS
+		}
 	);
 }
-gulp.task(buildTests);
+
+gulp.task(
+	"buildTestsTSC",
+	gulp.series(
+		buildTests,
+		gulp.parallel(
+			moveTestsCJS,
+			installTestsCJS,
+			moveTestsMJS,
+			installTestsMJS,
+			moveTestsPKGS,
+			installTestsPKGS
+		)
+	)
+);
 
 function buildUntilTests(cb) {
 	return exec(
--- tests/pkgs.spec.ts
+++ tests/pkgs.spec.ts
@@ -7,68 +7,56 @@ import words from "../src/word.js";
 import { toNumber } from "../src/tools.js";
 import fa_IR from "../src/languages/i18n/fa_IR";
 import ckb_IR from "../src/languages/l10n/ckb_IR";
+import { setTimeout } from "node:timers";
 
 const dataYear = 1402;
 
-describe("test CommonJS (CJS)", () => {
+describe("test CommonJS (CJS) && ES Modules (ESM) && exports pkgs", () => {
 	let dataCJS: any;
+	let dataMJS: any;
+	let dataPKGS: any;
 
 	before(function (done) {
 		exec(
-			`node dist/tests/cjs/dev.cjs ${dataYear}`,
+			`(node dist/tests/cjs/test.cjs ${dataYear})`,
 			function (error, stdout, stderr) {
 				if (error) done(error); // Handle errors.
+				setTimeout("test", 5000);
 				dataCJS = stdout.trim();
-				done();
 				// console.log(`stdout: ${stdout}`);
 				// console.error(`stderr: ${stderr}`);
 			}
 		);
+
+		exec(
+			`(node dist/tests/mjs/test.mjs ${dataYear})`,
+			function (error, stdout, stderr) {
+				if (error) done(error); // Handle errors.
+				dataMJS = stdout.trim();
+				// console.log(`stdout: ${stdout}`);
+				// console.error(`stderr: ${stderr}`);
+			}
+		);
+
+		exec(
+			`(node dist/tests/pkgs/test.js ${dataYear})`,
+			function (error, stdout, stderr) {
+				if (error) done(error); // Handle errors.
+				dataPKGS = stdout.trim();
+				// console.log(`stdout: ${stdout}`);
+				// console.error(`stderr: ${stderr}`);
+			}
+		);
+		done();
 	});
 
 	it("run CJS", () => {
 		assert.equal(dataCJS, new SHDate(dataYear).toDateString());
 	});
-});
-
-describe("test ES Modules (ESM)", () => {
-	let dataMJS: any;
-
-	before(function (done) {
-		exec(
-			`node dist/tests/mjs/dev.mjs ${dataYear}`,
-			function (error, stdout, stderr) {
-				if (error) done(error); // Handle errors.
-				dataMJS = stdout.trim();
-				done();
-				// console.log(`stdout: ${stdout}`);
-				// console.error(`stderr: ${stderr}`);
-			}
-		);
-	});
-
-	it("run ESM", () => {
+	it("run ES Modules (ESM)", () => {
 		assert.equal(dataMJS, new SHDate(dataYear).toDateString());
 	});
-});
-
-describe("test exports pkgs", () => {
-	let dataPKGS: any;
-
-	before(function (done) {
-		exec(
-			`node dist/tests/pkgs/test.js ${dataYear}`,
-			function (error, stdout, stderr) {
-				if (error) done(error); // Handle errors.
-				dataPKGS = stdout.trim();
-				done();
-				// console.log(`stdout: ${stdout}`);
-				// console.error(`stderr: ${stderr}`);
-			}
-		);
-	});
-
-	it("run pkgs", () => {
+	it("run exports pkgs", () => {
 		assert.deepEqual(dataPKGS, [
 			SHDate.VERSION,
 			words.word_language,
