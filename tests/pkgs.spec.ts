8c0db8bb3a77f1eb82c1b34acbf907eecac565a9
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2023-10-24 22:55:06 +0330

    refactor(gulp): بازنویسی gulp
    Signed-off-by: MD Amanalikhani <md.akhi.ir@gmail.com>

--- tests/pkgs.spec.ts
+++ tests/pkgs.spec.ts
@@ -7,27 +7,24 @@ import words from "../src/word.js";
 import { toNumber } from "../src/tools.js";
 import fa_IR from "../src/languages/i18n/fa_IR";
 import ckb_IR from "../src/languages/l10n/ckb_IR";
-import { setTimeout } from "node:timers";
 
 const dataYear = 1402;
 
 describe("test CommonJS (CJS) && ES Modules (ESM) && exports pkgs", () => {
-	let dataCJS: any;
 	let dataMJS: any;
 	let dataPKGS: any;
+	let dataCJS: any;
 
 	before(function (done) {
 		exec(
 			`(node dist/tests/cjs/test.cjs ${dataYear})`,
 			function (error, stdout, stderr) {
 				if (error) done(error); // Handle errors.
-				setTimeout("test", 5000);
 				dataCJS = stdout.trim();
 				// console.log(`stdout: ${stdout}`);
 				// console.error(`stderr: ${stderr}`);
 			}
 		);
-
 		exec(
 			`(node dist/tests/mjs/test.mjs ${dataYear})`,
 			function (error, stdout, stderr) {
