54bbb56dd3504315d5e0c9f9a6c3a5e39f3c0383
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2023-10-26 19:03:28 +0330

    refactor(gulp): بازنویسی gulp
    Signed-off-by: MD Amanalikhani <md.akhi.ir@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ const pkg = require("./package.json");
 const { argv } = require("node:process");
 const { exec } = require("node:child_process");
 
+const env = process.env;
+
 const COPY_RIGHT =
 	"(C) 2015 - 2023 Open Source Matters,Inc. All right reserved.";
 
@@ -34,29 +36,7 @@ const INFO_DOCBLOCK_SHORT = [
 	`/** In the name of Allah. | ${pkg.name}@${pkg.version} | ${COPY_RIGHT} | ${pkg.license} License | ${LINK_DOCBLOCK_LONG} */`
 ].join("\n");
 
-/**
- * combine all .ts files into one
- */
-function MergeFilesBrowserTS() {
-	return gulp
-		.src(["./src/languages/**/*.ts", "./src/parser/**/*.ts", "src/*.ts"])
-		.pipe(concat("shdate.ts"))
-		.pipe(replace(/export default (function|class)/g, "$1"))
-		.pipe(replace(/import [a-zA-Z_]* from [0-9a-zA-Z_/\.\"]*;/g, " "))
-		.pipe(replace(/extends (Language)/g, "extends SHDate$1"))
-		.pipe(replace(/class ([a-z]{2,3}_[A-Z]{2})/g, "class SHDateLanguage_$1"))
-		.pipe(replace(/ ([a-z]{2,3}_[A-Z]{2})\./g, " SHDateLanguage_$1."))
-		.pipe(replace(/ Language_([a-z]{2,3}_[A-Z]{2})/g, " SHDateLanguage_$1"))
-		.pipe(replace(/\((Languages)\)/g, "(SHDate$1)"))
-		.pipe(replace(/class (Language|Word)/g, "class SHDate$1"))
-		.pipe(replace(/(Language\.|Word\.)/g, "SHDate$1"))
-		.pipe(replace(/enum (Language)/g, "enum SHDate$1"))
-		.pipe(replace(/ Languages.([a-z]{2,3}_[A-Z]{2})/g, " SHDateLanguages.$1"))
-		.pipe(replace(/SH(Parser|Lexer|Token)/g, "SHDate$1"))
-		.pipe(gulp.dest("src/browser"));
-}
-
-function setDocBlockDescription() {
+function replaceDocBlockInfo() {
 	/**
 	 * In the name of Allah, the Beneficent, the Merciful. | \* In the name of Allah, the Beneficent, the Merciful\./im;
 	 */
@@ -104,14 +84,87 @@ function setDocBlockDescription() {
 		.pipe(gulp.dest("src/"));
 }
 
-function moveDotDToTypes(cb) {
+function cleanCompile(cb) {
+	return del(["dist", "src/browser"], cb);
+}
+gulp.task(cleanCompile);
+
+/**
+ * combine all .ts files into one
+ */
+function concatBrowserTS() {
+	return gulp
+		.src(["./src/languages/**/*.ts", "./src/parser/**/*.ts", "src/*.ts"])
+		.pipe(concat("shdate.ts"))
+		.pipe(replace(/export default (function|class)/g, "$1"))
+		.pipe(replace(/import [a-zA-Z_]* from [0-9a-zA-Z_/\.\"]*;/g, " "))
+		.pipe(replace(/extends (Language)/g, "extends SHDate$1"))
+		.pipe(replace(/class ([a-z]{2,3}_[A-Z]{2})/g, "class SHDateLanguage_$1"))
+		.pipe(replace(/ ([a-z]{2,3}_[A-Z]{2})\./g, " SHDateLanguage_$1."))
+		.pipe(replace(/ Language_([a-z]{2,3}_[A-Z]{2})/g, " SHDateLanguage_$1"))
+		.pipe(replace(/\((Languages)\)/g, "(SHDate$1)"))
+		.pipe(replace(/class (Language|Word)/g, "class SHDate$1"))
+		.pipe(replace(/(Language\.|Word\.)/g, "SHDate$1"))
+		.pipe(replace(/enum (Language)/g, "enum SHDate$1"))
+		.pipe(replace(/ Languages.([a-z]{2,3}_[A-Z]{2})/g, " SHDateLanguages.$1"))
+		.pipe(replace(/SH(Parser|Lexer|Token)/g, "SHDate$1"))
+		.pipe(gulp.dest("src/browser"));
+}
+
+function makeTypes(cb) {
 	return (
 		gulp.src("dist/cjs/**/*.d.ts").pipe(gulp.dest("dist/types")) &&
 		del(["dist/cjs/**/*.d.ts"], cb)
 	);
 }
 
-function settingBrowserJS() {
+function compileCjs(cb) {
+	//"tsc:cjs"
+	return exec(
+		'tsc --project tsconfig.json  && echo {"type":"commonjs"} > dist/cjs/package.json',
+		function (err, stdout, stderr) {
+			// console.log(stdout);
+			// console.log(stderr);
+			cb(err);
+		}
+	);
+}
+
+function compileMjs(cb) {
+	//"tsc:mjs"
+	return exec(
+		'tsc --project tsconfig.mjs.json && echo {"type":"module"} > dist/mjs/package.json',
+		function (err, stdout, stderr) {
+			// console.log(stdout);
+			// console.log(stderr);
+			cb(err);
+		}
+	);
+}
+
+function compileBrowser(cb) {
+	//"tsc:browser"
+	return exec(
+		"tsc --project tsconfig.browser.json",
+		function (err, stdout, stderr) {
+			// console.log(stdout);
+			// console.log(stderr);
+			cb(err);
+		}
+	);
+}
+
+function makeBrowserTypes() {
+	return gulp
+		.src(["src/browser/*.ts", "dist/browser/*.d.ts"])
+		.pipe(gulp.dest("dist/types"));
+}
+
+function cleanBrowserTS(cb) {
+	return del(["src/browser", "dist/browser/*.d.ts"], cb);
+}
+
+function compressBrowserJS() {
 	return gulp
 		.src("dist/browser/shdate.js", { sourcemaps: true })
 		.pipe(babel({ presets: ["@babel/env"] }))
@@ -124,74 +177,44 @@ function settingBrowserJS() {
 		.pipe(gulp.dest("./dist/browser", { sourcemaps: "." }));
 }
 
-function deleteMergeFilesBrowserTS(cb) {
-	return del(["src/browser", "dist/browser/*.d.ts"], cb);
-}
-
-function moveDotDBrowserTSToTypes() {
-	return gulp
-		.src(["src/browser/*.ts", "dist/browser/*.d.ts"])
-		.pipe(gulp.dest("dist/types"));
-}
-
-function buildCjs(cb) {
-	//"tsc:cjs"
-	return exec(
-		'tsc --project tsconfig.json  && echo {"type":"commonjs"}>dist/cjs/package.json',
-		function (err, stdout, stderr) {
-			// console.log(stdout);
-			// console.log(stderr);
-			cb(err);
-		}
-	);
-}
-
-function buildMjs(cb) {
-	//"tsc:mjs"
-	return exec(
-		'tsc --project tsconfig.mjs.json && echo {"type":"module"}>dist/mjs/package.json',
-		function (err, stdout, stderr) {
-			// console.log(stdout);
-			// console.log(stderr);
-			cb(err);
-		}
-	);
-}
-
-function buildBrowser(cb) {
-	//"tsc:browser"
-	return exec(
-		" tsc --project tsconfig.browser.json",
-		function (err, stdout, stderr) {
-			// console.log(stdout);
-			// console.log(stderr);
-			cb(err);
-		}
-	);
-}
-
-gulp.task(
-	"buildTSC",
-	gulp.parallel(
-		buildCjs,
-		moveDotDToTypes,
-		buildMjs,
-		MergeFilesBrowserTS,
-		buildBrowser
+/**
+ * Run default.
+ */
+exports.default = gulp.task(
+	"build",
+	gulp.series(
+		cleanCompile,
+		gulp.parallel(
+			compileCjs,
+			makeTypes,
+			compileMjs,
+			concatBrowserTS,
+			compileBrowser
+		),
+		compressBrowserJS,
+		makeBrowserTypes,
+		cleanBrowserTS,
+		replaceDocBlockInfo
 	)
 );
 
-function devClean(cb) {
+function cleanDev(cb) {
 	return del(["dist/src", "dist/tests", "tests/tst.out"], cb);
 }
-gulp.task(devClean);
 
-function buildClean(cb) {
-	return del(["dist", "src/browser"], cb);
+function compileTest(cb) {
+	return exec(
+		"tsc --project tsconfig.test.json",
+		function (err, stdout, stderr) {
+			// console.log(stdout);
+			// console.log(stderr);
+			cb(err);
+		}
+	);
 }
-gulp.task(buildClean);
+gulp.task(compileTest);
 
-function moveTestsCJS(cb) {
+function copyTestCJS(cb) {
 	return gulp.src(["tests/cjs/*"]).pipe(gulp.dest("dist/tests/cjs"));
 }
 function installTestsCJS(cb) {
@@ -204,7 +227,7 @@ function installTestsCJS(cb) {
 		}
 	);
 }
-function moveTestsMJS(cb) {
+function copyTestMJS(cb) {
 	return gulp.src(["tests/mjs/*"]).pipe(gulp.dest("dist/tests/mjs"));
 }
 function installTestsMJS(cb) {
@@ -217,7 +240,7 @@ function installTestsMJS(cb) {
 		}
 	);
 }
-function moveTestsPKGS(cb) {
+function copyTestPKG(cb) {
 	return gulp.src(["tests/pkgs/*"]).pipe(gulp.dest("dist/tests/pkgs"));
 }
 function installTestsPKGS(cb) {
@@ -230,56 +253,21 @@ function installTestsPKGS(cb) {
 		}
 	);
 }
-function buildTests(cb) {
-	return exec(
-		"tsc --project tsconfig.test.json",
-		function (err, stdout, stderr) {
-			// console.log(stdout);
-			// console.log(stderr);
-			cb(err);
-		}
-	);
-}
 
 gulp.task(
-	"buildTestsTSC",
+	"test",
 	gulp.series(
-		buildTests,
+		cleanDev,
+		compileTest,
 		gulp.parallel(
-			moveTestsCJS,
-			installTestsCJS,
-			moveTestsMJS,
-			installTestsMJS,
-			moveTestsPKGS,
-			installTestsPKGS
+			gulp.series(copyTestCJS, installTestsCJS),
+			gulp.series(copyTestMJS, installTestsMJS),
+			gulp.series(copyTestPKG, installTestsPKGS)
 		)
 	)
 );
 
-function buildUntilTests(cb) {
-	return exec(
-		"npm run tsc:test && node dist/tests/build-test.js > tests/build-test.log",
-		function (err, stdout, stderr) {
-			// console.log(stdout);
-			// console.log(stderr);
-			cb(err);
-		}
-	);
-}
-gulp.task(buildUntilTests);
-
-/**
- * Run default.
- */
-exports.default = gulp.series(
-	settingBrowserJS,
-	moveDotDBrowserTSToTypes,
-	deleteMergeFilesBrowserTS
-);
-// exports.deleteMergeTS = gulp.series();
-exports.MergeFilesBrowserTS = MergeFilesBrowserTS;
-exports.moveDotDToTypes = moveDotDToTypes;
-exports.setDocBlockDescription = setDocBlockDescription;
+gulp.task("testRelease", gulp.series(cleanDev, compileTest));
 
 // buildUntilTests.displayName = 'clean:all';
 // buildUntilTests.description = 'Build the project';
